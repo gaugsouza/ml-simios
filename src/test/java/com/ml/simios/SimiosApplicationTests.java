78731f448a80c6287e3c72b5267d919da684d544
gaugsouza <guilhermeguis@outlook.com>
2021-04-05 22:40:53 -0300

    repo init

---
+++ src/test/java/com/ml/simios/SimiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.simios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
